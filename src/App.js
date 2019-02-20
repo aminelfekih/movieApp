ca0a56e1fc9f94f49edb634c503ad0e4e6b54923
unknown <elfekihamine@gmail.com>
2019-02-20 09:45:01 +0100

    add pagination

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ class App extends Component {
     this.state = {
         listMovies: Object.values(movies),
         categories: [],
-        filter: 'All'
+        filter: 'All',
+        currentPage: 1,
+        cardsPerPage: 12
     };
     this.deleteMovie = this.deleteMovie.bind(this);
   }
@@ -43,23 +45,76 @@ class App extends Component {
     const categories = this.state.categories
     if (categories.length > 0) {
       return(
-        <div className="multiselect">
-        <select name="select" onChange={(e) => {this.setState({filter: e.target.value})}}>
+        <div className="category">
+        categories 
+        <select name="selectCategory" style={{marginLeft: '5px'}} onChange={(e) => {this.setState({filter: e.target.value})}}>
           <option value="All">All</option>
-          {categories.map((category) => <option value={category}>{category}</option>)}
+          {categories.map((category) => <option value={category} key={category}>{category}</option>)}
         </select>
        </div>
      )
     }
   }
+  elementsPerPage() {
+    return(
+      <div className="perPage" style={{marginTop: '10px'}}>
+        Size per page 
+        <select name="selectPerPage" style={{marginLeft: '10px'}} onChange={(e) => {this.setState({cardsPerPage: Number(e.target.value)})}}>
+          <option value="12" key="12">12</option>
+          <option value="8" key="8">8</option>
+          <option value="4" key="4">4</option>
+        </select>
+       </div>
+    )
+  }
+  pagination() {
+    const { listMovies, cardsPerPage, filter } = this.state;
+    const filtredMovies = this.filterByCategory(listMovies,filter)
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(filtredMovies.length / cardsPerPage); i++) {
+      pageNumbers.push(i);
+    }
+    return(
+      <div className="pagination">
+          {pageNumbers.map(
+            number => {
+              return (
+                <button
+                  key={number}
+                  id={number}
+                  onClick={(e) => {this.setState({currentPage: Number(e.target.id)})}}
+                >
+                  {number}
+                </button>
+            )}
+          )}
+      </div>
+    )
+  }
+  filterByCategory(movies, filter) {
+    if (filter === "All") {return movies}
+    else {
+        return movies.filter((movie) => {
+            return movie.category === filter
+        })
+    }
+  }
   
   render() {
+    debugger
+    const { filter, listMovies, currentPage, cardsPerPage } = this.state;
+    const filtredMovies = this.filterByCategory(listMovies,filter)
+    const indexOfLastCard = currentPage * cardsPerPage;
+    const indexOfFirstCard = indexOfLastCard - cardsPerPage;
+    const paginetedMovies = filtredMovies.slice(indexOfFirstCard, indexOfLastCard);
     return (
       <div className="App">
         {this.multiselect()}
-        <List movies={this.state.listMovies}
+        {this.elementsPerPage()}
+        <List movies={paginetedMovies}
           deleteHandle={this.deleteMovie}
-          filter={this.state.filter}></List>
+          filter={filter}></List>
+        {this.pagination()}
       </div>
     );
   }
--- src/components/list/List.js
+++ src/components/list/List.js
@@ -3,20 +3,11 @@ import Card from '../card/Card';
 import './List.css';
 
 class List extends Component {
-  filterByCategory(movies, filter) {
-    if (filter === "All") {return movies}
-    else {
-        return movies.filter((movie) => {
-            return movie.category === filter
-        })
-    }
-  }
   render() {
-    const {movies, filter, deleteHandle} = this.props
-    const filtredMovies = this.filterByCategory(movies,filter)
+    const {movies, deleteHandle} = this.props;
     return (
       <div className="List">
-        {filtredMovies.map((movie) => <Card movie={movie} key={movie.id} deleteHandle={deleteHandle}></Card>)}
+        {movies.map((movie) => <Card movie={movie} key={movie.id} deleteHandle={deleteHandle}></Card>)}
       </div>
     );
   }
